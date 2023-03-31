d64b178b8488a8a9923b5e23e95db99338d2b45e
ahmedhsultan <a.hossam@kemet.com.eg>
2023-03-31 14:57:21 +0200

    start

---
+++ src/main/java/com/example/demo/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "home", value = "/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("Helloh");
+    }
+}
