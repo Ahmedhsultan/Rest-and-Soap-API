28222bb9068422d06cfe4e16cf2e5322bbd05ac7
ahmedhsultan <a.hossam@kemet.com.eg>
2023-04-01 03:10:54 +0200

    exception handeling

---
+++ src/main/java/com/example/demo/webserviceies/soap/exception/SoapException.java
@@ -0,0 +1,16 @@
+package com.example.demo.webserviceies.soap.exception;
+
+import jakarta.xml.ws.WebFault;
+
+@WebFault
+public class SoapException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public SoapException(String message) {
+        super(message);
+    }
+
+    public String getFaultInfo() {
+        return this.getMessage();
+    }
+}
