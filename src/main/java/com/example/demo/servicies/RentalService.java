677ab8898dc3a1d12ff0c972a99468965d365212
ahmedhsultan <a.hossam@kemet.com.eg>
2023-04-08 09:36:58 +0200

    rental create method

--- src/main/java/com/example/demo/servicies/RentalService.java
+++ src/main/java/com/example/demo/servicies/RentalService.java
@@ -1,4 +1,50 @@
 package com.example.demo.servicies;
 
+import com.example.demo.repository.entities.*;
+import com.example.demo.repository.repos.*;
+import com.example.demo.webserviceies.rest.DTOs.PaymentDTO;
+import com.example.demo.webserviceies.rest.DTOs.RentalDTO;
+import jakarta.persistence.PersistenceException;
+import org.modelmapper.ModelMapper;
+
+import java.time.Instant;
+
 public class RentalService {
+    private CustomerRepo customerRepo;
+    private StaffRepo staffRepo;
+    private RentalRepo rentalRepo;
+    private InventoryRepo inventoryRepo;
+    private ModelMapper modelMapper;
+    public RentalService(){
+        this.customerRepo = new CustomerRepo();
+        this.staffRepo = new StaffRepo();
+        this.rentalRepo = new RentalRepo();
+        this.inventoryRepo = new InventoryRepo();
+        this.modelMapper = new ModelMapper();
+    }
+
+    public Rental create(RentalDTO rentalDTO) throws PersistenceException {
+        //Fetch film and store from db
+        Staff staff = staffRepo.getByName("firstName", rentalDTO.getStaffFirstName());
+        Inventory inventory = inventoryRepo.getById(rentalDTO.getInventoryId());
+        Customer customer = customerRepo.getByName("firstName", rentalDTO.getCustomerFirstName());
+
+        //Create rental
+        Rental rental = new Rental();
+        rental.setInventory(inventory);
+        rental.setRentalDate(Instant.now());
+        rental.setCustomer(customer);
+        rental.setLastUpdate(Instant.now());
+        rental.setStaff(staff);
+        rental.setReturnDate(Instant.parse(rentalDTO.getReturnDateInstance()));
+
+        //Save this rental
+        try {
+            rentalRepo.save(rental);
+        }catch (PersistenceException persistenceException){
+            throw new PersistenceException("Can't save this rental!!");
+        }
+
+        return rental;
+    }
 }
--- src/main/java/com/example/demo/webserviceies/rest/controllers/RentalController.java
+++ src/main/java/com/example/demo/webserviceies/rest/controllers/RentalController.java
@@ -1,4 +1,21 @@
 package com.example.demo.webserviceies.rest.controllers;
 
+import com.example.demo.servicies.RentalService;
+import com.example.demo.webserviceies.rest.DTOs.RentalDTO;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("rental")
 public class RentalController {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(RentalDTO rentalDTO){
+        RentalService rentalService = new RentalService();
+        rentalService.create(rentalDTO);
+
+        return Response.ok().build();
+    }
 }
