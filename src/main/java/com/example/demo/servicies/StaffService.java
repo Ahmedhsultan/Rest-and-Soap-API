ceeca24dc56822bb394cbe7c6602400401478915
ahmedhsultan <a.hossam@kemet.com.eg>
2023-04-08 09:44:10 +0200

    staff create method

--- src/main/java/com/example/demo/servicies/StaffService.java
+++ src/main/java/com/example/demo/servicies/StaffService.java
@@ -1,4 +1,52 @@
 package com.example.demo.servicies;
 
+import com.example.demo.repository.entities.Actor;
+import com.example.demo.repository.entities.Address;
+import com.example.demo.repository.entities.Staff;
+import com.example.demo.repository.entities.Store;
+import com.example.demo.repository.repos.ActorRepo;
+import com.example.demo.repository.repos.AddressRepo;
+import com.example.demo.repository.repos.StaffRepo;
+import com.example.demo.repository.repos.StoreRepo;
+import com.example.demo.webserviceies.rest.DTOs.ActorDTO;
+import com.example.demo.webserviceies.rest.DTOs.StaffDTO;
+import jakarta.persistence.PersistenceException;
+import org.modelmapper.ModelMapper;
+
+import java.time.Instant;
+
 public class StaffService {
+    private StaffRepo staffRepo;
+    private AddressRepo addressRepo;
+    private StoreRepo storeRepo;
+    private ModelMapper modelMapper;
+
+    public StaffService(){
+        //Create objects from repositories
+        this.modelMapper = new ModelMapper();
+        this.staffRepo = new StaffRepo();
+        this.storeRepo = new StoreRepo();
+        this.addressRepo = new AddressRepo();
+    }
+
+    public Staff create(StaffDTO staffDTO) throws PersistenceException {
+        //Fetch Address and store from db
+        Address address = addressRepo.getByName("address", staffDTO.getAddress());
+        Store store = storeRepo.getById(staffDTO.getStoreId());
+
+        //Create object of staff
+        Staff staff = modelMapper.map(staffDTO, Staff.class);
+        staff.setStore(store);
+        staff.setAddress(address);
+        staff.setLastUpdate(Instant.now());
+
+        //Save this staff
+        try {
+            staffRepo.save(staff);
+        }catch (PersistenceException persistenceException){
+            throw new PersistenceException("Can't save this staff!!");
+        }
+
+        return staff;
+    }
 }
--- src/main/java/com/example/demo/webserviceies/rest/controllers/StaffController.java
+++ src/main/java/com/example/demo/webserviceies/rest/controllers/StaffController.java
@@ -1,4 +1,21 @@
 package com.example.demo.webserviceies.rest.controllers;
 
+import com.example.demo.servicies.StaffService;
+import com.example.demo.webserviceies.rest.DTOs.StaffDTO;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("staff")
 public class StaffController {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(StaffDTO staffDTO){
+        StaffService staffService = new StaffService();
+        staffService.create(staffDTO);
+
+        return Response.ok().build();
+    }
 }
