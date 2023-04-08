c829706fca7dd864cf86260347de1ffedeeb9e72
ahmedhsultan <a.hossam@kemet.com.eg>
2023-04-08 09:28:21 +0200

    Payment

--- src/main/java/com/example/demo/servicies/PaymentService.java
+++ src/main/java/com/example/demo/servicies/PaymentService.java
@@ -1,4 +1,49 @@
 package com.example.demo.servicies;
 
+import com.example.demo.repository.entities.*;
+import com.example.demo.repository.repos.*;
+import com.example.demo.webserviceies.rest.DTOs.PaymentDTO;
+import jakarta.persistence.PersistenceException;
+import org.modelmapper.ModelMapper;
+
+import java.time.Instant;
+
 public class PaymentService {
+    private CustomerRepo customerRepo;
+    private StaffRepo staffRepo;
+    private RentalRepo rentalRepo;
+    private PaymentRepo paymentRepo;
+    private ModelMapper modelMapper;
+    public PaymentService(){
+        this.customerRepo = new CustomerRepo();
+        this.staffRepo = new StaffRepo();
+        this.rentalRepo = new RentalRepo();
+        this.paymentRepo = new PaymentRepo();
+        this.modelMapper = new ModelMapper();
+    }
+
+    public Payment create(PaymentDTO paymentDTO) throws PersistenceException {
+        //Fetch film and store from db
+        Staff staff = staffRepo.getByName("firstName", paymentDTO.getStaffFirstName());
+        Rental rental = rentalRepo.getById(paymentDTO.getRentalId());
+        Customer customer = customerRepo.getByName("firstName", paymentDTO.getCustomerFirstName());
+
+        //Create payment
+        Payment payment = new Payment();
+        payment.setPaymentDate(Instant.now());
+        payment.setCustomer(customer);
+        payment.setAmount(paymentDTO.getAmount());
+        payment.setRental(rental);
+        payment.setLastUpdate(Instant.now());
+        payment.setStaff(staff);
+
+        //Save this payment
+        try {
+            paymentRepo.save(payment);
+        }catch (PersistenceException persistenceException){
+            throw new PersistenceException("Can't save this payment!!");
+        }
+
+        return payment;
+    }
 }
--- src/main/java/com/example/demo/webserviceies/rest/controllers/PaymentController.java
+++ src/main/java/com/example/demo/webserviceies/rest/controllers/PaymentController.java
@@ -1,4 +1,21 @@
 package com.example.demo.webserviceies.rest.controllers;
 
+import com.example.demo.servicies.PaymentService;
+import com.example.demo.webserviceies.rest.DTOs.PaymentDTO;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("payment")
 public class PaymentController {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(PaymentDTO paymentDTO){
+        PaymentService paymentService = new PaymentService();
+        paymentService.create(paymentDTO);
+
+        return Response.ok().build();
+    }
 }
