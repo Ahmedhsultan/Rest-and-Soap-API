2e36d1a2dc5e3e1a12468b8d2eb430676b47b8a7
ahmedhsultan <a.hossam@kemet.com.eg>
2023-04-17 04:45:48 +0200

    typo error in CustomerController

--- src/main/java/com/example/demo/webservices/rest/controllers/CustomerController.java
+++ src/main/java/com/example/demo/webservices/rest/controllers/CustomerController.java
@@ -3,11 +3,11 @@ package com.example.demo.webservices.rest.controllers;
 
 import com.example.demo.servicies.CustomerService;
 import com.example.demo.webservices.rest.DTOs.requests.CustomerDTOReq;
-import com.example.demo.webservices.rest.DTOs.resources.CategoryDTOResp;
+import com.example.demo.webservices.rest.DTOs.resources.CustomerDTOResp;
 import jakarta.ws.rs.Path;
 
 @Path("customer")
-public class CustomerController extends BaseController<CategoryDTOResp, CustomerService, CustomerDTOReq>{
+public class CustomerController extends BaseController<CustomerDTOResp, CustomerService, CustomerDTOReq>{
     public CustomerController() throws InstantiationException, IllegalAccessException {
     }
 }
