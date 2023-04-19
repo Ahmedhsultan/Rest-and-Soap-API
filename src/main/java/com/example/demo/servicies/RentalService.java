d806d3e1767dfeaedfd7f28495952b22faa1a307
ahmedhsultan <a.hossam@kemet.com.eg>
2023-04-19 02:47:18 +0200

    rental business logic

--- src/main/java/com/example/demo/servicies/RentalService.java
+++ src/main/java/com/example/demo/servicies/RentalService.java
@@ -10,6 +10,8 @@ import jakarta.persistence.PersistenceException;
 import org.modelmapper.ModelMapper;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RentalService extends BaseService<Rental, RentalDTOResp, RentalRepo, RentalDTOReq>{
     private ModelMapper modelMapper;
@@ -19,15 +21,21 @@ public class RentalService extends BaseService<Rental, RentalDTOResp, RentalRepo
 
     @Override
     public Rental post(RentalDTOReq rentalDTOReq) throws PersistenceException {
+
         //Fetch film and store from db
         Staff staff = UnitOfWork.getInstance().getStaffRepo().find(rentalDTOReq.getStaffId());
         Inventory inventory = UnitOfWork.getInstance().getInventoryRepo().find(rentalDTOReq.getInventoryId());
         Customer customer = UnitOfWork.getInstance().getCustomerRepo().find(rentalDTOReq.getCustomerId());
 
+        //Check if inventory rented or customer inactive
+        if(!customer.getActive())
+            throw new OperationFaildException("Customer isn't active!!");
+        if(isRented(inventory))
+            throw new OperationFaildException("Inventory is rented!!");
+
         //Create rental
         Rental rental = new Rental();
         rental.setInventory(inventory);
-        rental.setRentalDate(Instant.now());
         rental.setCustomer(customer);
         rental.setStaff(staff);
         rental.setReturnDate(Instant.parse(rentalDTOReq.getReturnDateInstance()));
@@ -40,5 +48,19 @@ public class RentalService extends BaseService<Rental, RentalDTOResp, RentalRepo
         }
 
         return rental;
+    }
+
+    public boolean isRented(Inventory inventory){
+        try {
+            //Get all rentals history for this inventory
+            List<Rental> rentalsHistory = UnitOfWork.getInstance().getRentalRepo().find("inventory", inventory);
+            //Check if one of them still rented and the rented date after now
+            List<Rental> rentals = rentalsHistory.stream().filter(x -> x.getRentalDate().isAfter(Instant.now())).toList();
+            if (rentals.stream().count()>0)
+                return true;
+        }catch (IndexOutOfBoundsException exception){
+            return false;
+        }
+        return false;
     }
 }
